cfca4262868f3de342fcb2b01a48c8b85202b0e9
Sakshi Goral <sakshigoral37@gmail.com>
2025-11-15 14:11:44 +0000

    make share that all features work propely

--- src/app/(main)/assistant/page.tsx
+++ src/app/(main)/assistant/page.tsx
@@ -1,10 +1,5 @@
 import ChatInterface from '@/components/assistant/chat-interface';
-import { DataProvider } from '@/contexts/data-context';
 
 export default function AssistantPage() {
-  return (
-    <DataProvider>
-      <ChatInterface />
-    </DataProvider>
-  );
+  return <ChatInterface />;
 }
